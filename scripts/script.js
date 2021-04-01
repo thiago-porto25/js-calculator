6516d7b7ba52f73d89f3f4bf1ec14b537ec1b713
thiago-porto25 <thiago_porto25@hotmail.com>
2021-04-01 08:42:28 -0300

    add functionality to the calculator, display numbers when pressed, undo and clear button work

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,97 @@
 /*
-1- user inputs a number or a negative sign and a number;
+if the display has a floting point, disable that button;
 
-    - when the user presses a button, send that to a <p> in the calcDisplay, the text is aligned left;
+add a number maximum;
 
-2- user inputs a mathematical operator;
+don't let the user repeat the operator one after the other;
 
-    - when the user hits a operatior after that store what was sent to <p> in a variable as a number.
-    - when the user hits a operator store the operator in a variable;
+check for bugs if user presses "=" early;
 
-3- user inputs another number;
-
-    - when the user presses a button, send that to a <p> in the calcDisplay, the text is aligned left;
-
-4- after this user can input more numbers and operators up to a max or hit the equals sign;
-5- after equals sign is hit display the result of the operation;
-
-    - remove the content of <p> and send it the result of operate().
-
-
-
-- user can hit clear at any moment and it restarts everything;
-
-- in the input for the num I need a RegEx to only take numbers or a minus sign;
-
-- ter um eventListener para um click nos .keys e adcionar um classe .clickedKey e um eventLister para transitionend
- e voltar ao normal 
+add keyboard support;
 */
 
+const keys = document.querySelector("#calcKeys");
+const display = document.querySelector("#calcDisplay p");
+const button = Array.from(document.querySelectorAll("button"));
+
+console.log(display.textContent)
+
+keys.addEventListener("click", e => {
+
+    if (e.target.matches("button")) {
+
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+
+        key.classList.add("clickedKey");
+        key.addEventListener("transitionend", () => key.classList.remove("clickedKey"));
+
+        if (!action) {
+            if (displayedNum == "0") {
+                display.textContent = keyContent;
+            }
+            if (displayedNum != "0") {
+                display.textContent += keyContent;
+            }
+        }
+
+        if (action == "add" ||
+            action == "subtract" ||
+            action == "divide" ||
+            action == "multiply") {
+
+            let help = display.textContent;
+            let iadd = help.indexOf("+");
+            let isub = help.indexOf("-");
+            let imul = help.indexOf("*");
+            let idiv = help.indexOf("/");
+
+            display.textContent += " " + keyContent + " ";
+
+            if (help.slice(iadd, iadd + 1) == "+" ||
+                help.slice(isub, isub + 1) == "-" ||
+                help.slice(imul, imul + 1) == "*" ||
+                help.slice(idiv, idiv + 1) == "/") {
+                inputArray = display.textContent.split(" ");
+                operate(inputArray[0], inputArray[1], inputArray[2]);
+                display.textContent += " " + keyContent + " ";
+            }
+        }
+
+        if (action == "decimal") {
+            display.textContent += keyContent;
+        }
+        if (action == "undo") {
+            let test = display.textContent.slice(-2, -1);
+
+            if (test == "+" ||
+                test == "-" ||
+                test == "*" ||
+                test == "/") {
+                display.textContent = display.textContent.slice(0, -2);
+            }
+            if (test == " ") {
+                display.textContent = display.textContent.slice(0, -2);
+            }
+            else display.textContent = display.textContent.slice(0, -1);
+        }
+        if (action == "clear") {
+            display.textContent = "0";
+        }
+        if (action == "calculate") {
+            inputArray = display.textContent.split(" ");
+            operate(inputArray[0], inputArray[1], inputArray[2]);
+        }
+    }
+
+})
 
 
 
 function add(num1, num2) {
-    return num1 + num2;
+    return parseInt(num1 * 100) / 100 + parseInt(num2 * 100) / 100;
 }
 
 function subtract(num1, num2) {
@@ -51,6 +111,7 @@ function operate(num1, operator, num2) {
     let result;
 
     if (operator == "*") {
+        console.log(num1);
         result = multiply(num1, num2);
     }
 
@@ -67,5 +128,5 @@ function operate(num1, operator, num2) {
     }
     else return "ERROR";
 
-    return result;
+    display.textContent = Math.round(result * 100) / 100;
 }
